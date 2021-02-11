f2848caad4d73aeff6a73bad80afff3efdaeb228
jesarbov <jesarbov@gmail.com>
2021-02-11 09:39:39 +0100

    DiceController - update

--- Lesson02/Lesson02/Controllers/DiceController.cs
+++ Lesson02/Lesson02/Controllers/DiceController.cs
@@ -11,22 +11,19 @@ namespace Lesson02.Controllers
     {
         public IActionResult Index()
         {
-            return View();  // method of the Controller class, 
-                            // that returns an ActionResult object (View)
+            return View();  
         }
 
         // POST
         [HttpPost]
         public IActionResult Index(IFormCollection fc)
         {
-            // Create a new Random object 
-            Random rnd = new Random(); // rnd is a variable of type Random
+            Random rnd = new Random(); 
 
-            // Next(1, 7) (method) returns a random number between 1 and 7 (exclusive)
             int i = rnd.Next(1, 7);
 
-            ViewBag.Face = i; // ViewBag: Object; ViewBag.Face // Property 
-            return View(); // View is a method of the Controller class
+            ViewBag.Face = i; 
+            return View(); 
         }
     }
 }
