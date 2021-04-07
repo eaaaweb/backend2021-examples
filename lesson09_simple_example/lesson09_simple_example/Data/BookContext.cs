7657d4ffb09cbecf6aa568e3b2ae6096df9a6108
jesarbov <jesarbov@gmail.com>
2021-04-07 16:54:59 +0200

    Exercise 09 - simple example

---
+++ lesson09_simple_example/lesson09_simple_example/Data/BookContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication14.Models;
+
+namespace lesson09_simple_example.Data
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions<BookContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Fluent API mappings (if needed)
+            modelBuilder.Entity<Book>().ToTable("Book");
+            modelBuilder.Entity<Author>().ToTable("Author");
+
+            //Configure Book Column
+            modelBuilder.Entity<Book>()
+                        .Property(b => b.PublishedOn)
+                        .HasColumnType("datetime2");
+
+            // Add example data (optional)
+
+        }
+    }
+}
---
+++ lesson09_simple_example/lesson09_simple_example/Startup.cs
@@ -0,0 +1,52 @@
+using lesson09_simple_example.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace lesson09_simple_example_simple_example
+{
+    public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllersWithViews();
+        services.AddDbContext<BookContext>(options =>
+        options.UseSqlServer(Configuration.GetConnectionString("BookContext")));
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Authors}/{action=Index}/{id?}");
+        });
+    }
+}
+}
